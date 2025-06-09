1312a6a8e8c6cf7a2b8c2280b1c0f18b0d8303c6
Nexxxit <varsik.arsik@mail.ru>
2025-06-09 18:03:38 +0500

    feat: update MessageList for MobX

---
+++ src/components/messageList/MessageList.tsx
@@ -0,0 +1,18 @@
+import { messageStore } from "../../stores/messageStore";
+import Message from "../message/Message";
+import { observer } from "mobx-react-lite";
+
+export default observer(function MessageList() {
+  return (
+    <div className="col-span-6 row-span-3 flex flex-col gap-3 overflow-y-scroll p-3">
+      {messageStore.message.map((message) => (
+        <Message
+          key={message.id}
+          userName={message.userName}
+          messageText={message.messageText}
+          timestamp={message.timestamp}
+        />
+      ))}
+    </div>
+  );
+});
