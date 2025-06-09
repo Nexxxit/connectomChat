d05b1665f41d5609f2d7323e9b69bf59dd92651a
Nexxxit <varsik.arsik@mail.ru>
2025-06-09 23:49:48 +0500

    feat: add provider in App for MobX

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { Provider } from "mobx-react";
 import MessageComposer from "./components/messageComposer/MessageComposer";
 import MessageList from "./components/messageList/MessageList";
 import UserPickerMenu from "./components/userPickerMenu/UserPickerMenu";
+import { messageStore } from "./stores/messageStore";
 
 function App() {
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-6 grid-rows-3 border rounded-xl py-2 px-4 gap-2 h-screen w-3/4 bg-green-500/20">
-      <MessageList />
-      <UserPickerMenu />
-      <MessageComposer />
-    </div>
-    </div>
+    <Provider messageStore={messageStore}>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-6 grid-rows-3 border rounded-xl py-2 px-4 gap-2 h-screen w-3/4 bg-green-500/20">
+          <MessageList />
+          <UserPickerMenu />
+          <MessageComposer />
+        </div>
+      </div>
+    </Provider>
   );
 }
 
