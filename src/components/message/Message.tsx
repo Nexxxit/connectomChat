bab4f6dfd9a4deafee344676be67bf83ab7b8844
Nexxxit <varsik.arsik@mail.ru>
2025-06-10 17:08:20 +0500

    fix: correct date display in message

--- src/components/message/Message.tsx
+++ src/components/message/Message.tsx
@@ -1,4 +1,5 @@
 interface MessageProps {
+  userPicture: string;
   userName: string;
   messageText: string;
   timestamp: Date;
@@ -6,6 +7,7 @@ interface MessageProps {
 }
 
 export default function Message({
+  userPicture,
   userName,
   messageText,
   timestamp,
@@ -13,11 +15,21 @@ export default function Message({
   // const [addedFile, setAddedFile] = useState<File | null>()
 
   const formatDate = (date: Date) => {
-    const isoString = date.toISOString();
-    const [datePart, timePart] = isoString.split("T");
-    const [year, day, month] = datePart.split("-");
-    const time = timePart.split(".")[0];
-    return `${day}.${month}.${year} ${time}`;
+    // const isoString = date.toISOString();
+    // const [datePart, timePart] = isoString.split("T");
+    // const [year, day, month] = datePart.split("-");
+    // const time = timePart.split(".")[0];
+    // return `${day}.${month}.${year} ${time}`;
+
+    return date.toLocaleString('ru-RU', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    })
   };
 
   return (
@@ -26,13 +38,20 @@ export default function Message({
         userName === "Bot" ? "bg-red-300 me-10" : "bg-blue-300 ms-10"
       } rounded-3xl p-3 flex flex-col gap-3 shadow-xl`}
     >
-      <span
-        className={`text-${
-          userName === "Bot" ? "red" : "indigo"
-        }-700 font-bold`}
-      >
-        {userName}
-      </span>
+      <div className="flex items-center">
+        <img
+          className="rounded-full mr-2 w-8 h-8"
+          src={userPicture}
+          alt={`${userName} picture`}
+        />
+        <span
+          className={`${
+            userName === "Bot" ? "text-red-300" : "text-indigo-700"
+          } font-bold`}
+        >
+          {userName}
+        </span>
+      </div>
       {/* {addedFile && (
                 <div>
                     {addedFile}
