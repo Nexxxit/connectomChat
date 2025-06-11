60685d91d5bb61bec410d5bef173d713fc182409
Nexxxit <varsik.arsik@mail.ru>
2025-06-11 21:42:46 +0500

    feat: add CommandMenu

---
+++ src/components/commandMenu/CommandMenu.tsx
@@ -0,0 +1,10 @@
+export default function CommandMenu() {
+  return (
+    <div className="flex flex-col gap-3">
+      <button className="border bg-gray-200 p-2 cursor-pointer hover:bg-gray-400">*Что-то делаю*</button>
+      <button className="border bg-gray-200 p-2 cursor-pointer hover:bg-gray-400">*Что-то делаю*</button>
+      <button className="border bg-gray-200 p-2 cursor-pointer hover:bg-gray-400">*Что-то делаю*</button>
+      <button className="border bg-gray-200 p-2 cursor-pointer hover:bg-gray-400">*Что-то делаю*</button>
+    </div>
+  );
+}
--- src/components/messageComposer/MessageComposer.tsx
+++ src/components/messageComposer/MessageComposer.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { messageStore } from "../../stores/messageStore";
+import CommandMenu from "../commandMenu/CommandMenu";
 
 export default function MessageComposer() {
   const [messageText, setMessageText] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleSendMessage = () => {
     messageStore.addMessage(messageText);
@@ -17,46 +19,62 @@ export default function MessageComposer() {
   };
 
   return (
-    <div className="col-span-6 row-span-1 border rounded-xl p-3 flex gap-3 justify-between bg-indigo-700">
-      <label className="shadow p-1 rounded-full bg-white active:translate-y-1 transition duration-100 ease-in-out">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          fill="currentColor"
-          className="bi bi-paperclip"
-          viewBox="0 0 16 16"
-        >
-          <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
-        </svg>
+    <div className="col-span-6 row-span-1 shadow rounded-xl p-3 flex flex-col gap-4 bg-white">
+      <div className="flex gap-3">
+        <label className="p-1 cursor-pointer text-gray-700 rounded-full bg-white rotate-225 active:translate-y-1 transition duration-100 ease-in-out">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="currentColor"
+            className="bi bi-paperclip"
+            viewBox="0 0 16 16"
+          >
+            <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z" />
+          </svg>
+          <input className="shrink p-1 hidden" type="file" />
+        </label>
         <input
-          className="shrink border cursor-pointer p-1 hidden"
-          type="file"
+          className="w-full rounded-3xl px-2 grow-1 focus:outline-none bg-white"
+          type="text"
+          placeholder="Введите текст"
+          onChange={(e) => setMessageText(e.target.value)}
+          onKeyDown={handleKeyDown}
+          value={messageText}
         />
-      </label>
-      <input
-        className="w-full border rounded-3xl px-2 grow-1 bg-white shadow"
-        type="text"
-        placeholder="Введите текст"
-        onChange={(e) => setMessageText(e.target.value)}
-        onKeyDown={handleKeyDown}
-        value={messageText}
-      />
-      <button
-        onClick={handleSendMessage}
-        className="shrink border cursor-pointer p-1 rounded-full shadow bg-blue-700 active:translate-y-1 transition duration-100 ease-in-out"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="16"
-          fill="currentColor"
-          className="bi bi-send"
-          viewBox="0 0 16 16"
+        <button
+          type="button"
+          className="shrink cursor-pointer p-1 rounded-full text-gray-700 bg-white active:translate-y-1 transition duration-100 ease-in-out"
+          onClick={() => setShowMenu(!showMenu)}
         >
-          <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="24"
+            fill="currentColor"
+            className="bi bi-grid"
+            viewBox="0 0 16 16"
+          >
+            <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5z" />
+          </svg>
+        </button>
+        <button
+          onClick={handleSendMessage}
+          className="shrink cursor-pointer p-1 rounded-full text-blue-700 bg-white active:translate-y-1 transition duration-100 ease-in-out"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="24"
+            fill="currentColor"
+            className="bi bi-send-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+          </svg>
+        </button>
+      </div>
+      {showMenu && <CommandMenu />}
     </div>
   );
 }
