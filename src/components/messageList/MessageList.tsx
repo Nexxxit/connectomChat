4022763f9cd08017160f216cd1f88faa5f4e36f4
Nexxxit <varsik.arsik@mail.ru>
2025-06-12 16:30:24 +0500

    feat: add scroll to last message

--- src/components/messageList/MessageList.tsx
+++ src/components/messageList/MessageList.tsx
@@ -1,10 +1,29 @@
+import { useEffect, useRef } from "react";
 import { messageStore } from "../../stores/messageStore";
 import Message from "../message/Message";
 import { observer } from "mobx-react-lite";
 
 export default observer(function MessageList() {
+  const prevLengthRef = useRef(0);
+  const messageEndRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (messageStore.messages.length > prevLengthRef.current) {
+      if (messageEndRef.current) {
+        messageEndRef.current?.scrollTo({
+          top: messageEndRef.current.scrollHeight,
+          behavior: "smooth",
+        });
+      }
+    }
+    prevLengthRef.current = messageStore.messages.length;
+  }, [messageStore.messages]);
+
   return (
-    <div className="col-span-6 row-span-3 flex flex-col gap-3 overflow-y-scroll p-3">
+    <div
+      className="col-span-6 row-span-3 flex flex-col gap-3 overflow-y-scroll p-3 custom-scrollbar"
+      ref={messageEndRef}
+    >
       {messageStore.messages.map((message) => (
         <Message
           key={message.id}
